b1ceff87344e725cb3b5b3367525e432ee5a5dae
Colin Nelson <orthros@users.noreply.github.com>
2021-08-09 12:21:57 -0700

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,16 @@
+// sample-metadata:
+//   title: MySample
+//   description: "A super cool sample"
+// [START sample_one]
+def foo():
+  return 'foo'
+// [END sample_one]
+
+
+// sample-metadata:
+//   title: YourSample
+//   description: "Another cool sample"
+// [START sample_two]
+def bar():
+  return 'bar'
+// [END sample_two]
