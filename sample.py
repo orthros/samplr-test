65c9983a36e2376f26d3cc56a9e2ceca73d07f99
Colin Nelson <orthros@users.noreply.github.com>
2021-08-09 12:26:16 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -6,11 +6,3 @@ def foo():
   return 'foo'
 // [END sample_one]
 
-
-// sample-metadata:
-//   title: YourSample
-//   description: "Another cool sample"
-// [START sample_two]
-def bar():
-  return 'bar'
-// [END sample_two]
